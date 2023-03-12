956b1870213a3c237dccf67038e0bfbc4b45c6be
Mamun Riaz <mamunr412@gmail.com>
2023-03-12 21:30:37 +0600

    page setup

---
+++ components/HomeWrapper/HomeWrapper.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Footer from '../Footer/Footer'
+import Header from '../Header/Header'
+
+const HomeWrapper = () => {
+  return (
+    <div>
+<Header/>
+<Footer/>
+
+    </div>
+  )
+}
+
+export default HomeWrapper
